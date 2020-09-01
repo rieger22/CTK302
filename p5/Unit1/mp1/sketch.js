535bcf5c4360552b8de3f25f8aaa02cd6e492679
rieger22 <58442709+rieger22@users.noreply.github.com>
2020-09-01 11:25:00 -0500

    q

--- p5/Unit1/mp1/sketch.js
+++ p5/Unit1/mp1/sketch.js
@@ -1,7 +1,19 @@
 function setup() {
-
+  createCanvas(500, 500);
 }
 
 function draw() {
+  background('black');
+
+  if (mouseIsPressed) {
+    //background, shapes, colors
+    background('red');
+  } else {
+//normal stuff!
+
+  }
+ellipse(100, 100, 100, 100);
+
+text(mouseX + " , " + mouseY, 10, 20);
 
 }
