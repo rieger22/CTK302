8165929327a90fa9fab4a47a04418436f604762f
rieger22 <58442709+rieger22@users.noreply.github.com>
2020-09-01 11:06:53 -0500

    a

---
+++ p5/Unit1/circle/mp1.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+
+  if(mouseIsPressed) {
+    //background, shapes, colors
+
+  } else {
+//normal stuff!
+
+  }
+  background(100);
+  ellipse(100, 100, 100, 100) ;
+
+
+
+text(mouseX + " , " + mouseY, 10, 20);
+}
--- p5/Unit1/circle/sketch.js
+++
@@ -1,7 +0,0 @@
-function setup() {
-  createCanvas(500, 500);
-}
-
-function draw() {
-  ellipse(100, 100, 100, 100) ;
-}
