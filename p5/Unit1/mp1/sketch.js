82f74256257cc8c0f044a4a102a20491f7d7f676
rieger22 <58442709+rieger22@users.noreply.github.com>
2020-09-08 02:05:01 -0500

    1

--- p5/Unit1/mp1/sketch.js
+++ p5/Unit1/mp1/sketch.js
@@ -3,17 +3,46 @@ function setup() {
 }
 
 function draw() {
-  background('black');
+  background('blue');
 
 
   if (mouseIsPressed) {
     //background, shapes, colors
-    background('red');
+    background('#ff69b4');
+
+//head
+ellipse(250, 150, 100, 100);
+    fill('yellow')
+//body
+rect(225, 200, 50, 100);
+//left leg
+rect(225, 300, 15, 75);
+//right leg
+rect(260, 300, 15, 75);
+
+//left arm 1
+rect();
+//left arm 2
+
+//left arm 3
+
+//right arm 1
+
+//right arm 2
+
+//right arm 3
+
+arc()
+
   } else {
     //normal stuff!
+ellipse(250, 150, 100, 100);
+fill('white')
+  rect(225, 200, 50, 100);
+
+
 
   }
-  ellipse(100, 100, 100, 100);
 
   text(mouseX + " , " + mouseY, 10, 20);
 
