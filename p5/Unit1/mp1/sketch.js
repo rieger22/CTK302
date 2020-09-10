660a2263c47485d17bb1cf1eb1d2306af419833c
rieger22 <58442709+rieger22@users.noreply.github.com>
2020-09-10 10:30:38 -0500

    5

--- p5/Unit1/mp1/sketch.js
+++ p5/Unit1/mp1/sketch.js
@@ -3,55 +3,79 @@ function setup() {
 }
 
 function draw() {
-  background('grey');
+  background('#915118');
 
 
   if (mouseIsPressed) {
     //background, shapes, colors
     background('blue');
 
-//head
-ellipse(250, 150, 100, 100);
+    //head
+    fill('#ff69b4');
+    ellipse(250, 150, 100, 100);
+    //body
     fill('yellow');
-//body
-rect(225, 200, 50, 100);
-fill('#ff69b4');
-//left leg
-rect(225, 300, 15, 75);
-//right leg
-rect(260, 300, 15, 75);
+    rect(225, 200, 50, 100);
+    //left leg
+    fill('black');
+    rect(225, 300, 15, 75);
+    //right leg
+    rect(260, 300, 15, 75);
 
-//left arm 1
-rect(150, 200, 75, 10);
-//left arm 2
-rect(140, 230, 85, 10);
-//left arm 3
-rect(150, 260, 75, 10);
+    //left arm 1
+    fill('#009900');
+    rect(150, 200, 75, 10);
+    //left arm 2
+    rect(140, 230, 85, 10);
+    //left arm 3
+    rect(150, 260, 75, 10);
 
-//right arm 1
-rect(275, 200, 75, 10);
-//right arm 2
-rect(275, 230, 85, 10);
-//right arm 3
-rect(275, 260, 75, 10);
+    //right arm 1
+    rect(275, 200, 75, 10);
+    //right arm 2
+    rect(275, 230, 85, 10);
+    //right arm 3
+    rect(275, 260, 75, 10);
 
-//smile
-arc(250, 170, 50, 50, 0, PI);
+    //smile
+    fill('black');
+    arc(250, 170, 40, 20, 0, PI);
 
-//left eye
+    //left eye
+    fill('red');
+    ellipse(225, 140, 15, 10);
 
-//right eye
+    //right eye
+    fill('red');
+    ellipse(275, 140, 15, 10);
 
+    //building
+    fill('white');
+    rect(210, 440, 80, 100);
+    //roof
+  fill('grey');
+  triangle(210, 440, 250, 400, 290, 440);
+//door
+fill('#915118');
+rect(240, 470, 20, 30)
   } else {
     //normal stuff!
-ellipse(250, 150, 100, 100);
-fill('white')
-  rect(225, 200, 50, 100);
-
-
-
+//body
+    rect(225, 200, 50, 100, 20);
+    fill('white')
+//smile
+    arc(250, 230, 40, 20, 0, PI);
+//left eye
+      ellipse(240, 220, 15, 10);
+//right eye
+ellipse(260, 220, 15, 10);
   }
 
   text(mouseX + " , " + mouseY, 10, 20);
 
 }
+//9/5 2 hours planning
+//9/6 1 hour researching
+//9/7 1 hour coding
+//9/8 2 hours coding
+//9/9 2 hours tweaking
