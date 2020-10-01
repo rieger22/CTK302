dc57dce011b45be2fd83531d1b51b1154707d8d5
rieger22 <58442709+rieger22@users.noreply.github.com>
2020-10-01 11:38:17 -0500

    5

--- p5/Unit2/c2/sketch.js
+++ p5/Unit2/c2/sketch.js
@@ -1,6 +1,6 @@
 var mic;
 var vol;
-var c = 'red' ;
+var c = 'red';
 let x = 0;
 let state = 0;
 
@@ -18,26 +18,26 @@ function draw() {
   // get the sound input
   vol = (mic.getLevel()).toFixed(2); // returned level is between 0 and 1
 
-switch (state) {
-  case 0:
-  background('red');
-  text("shhhh!", 250, 250);
-  if (vol > .05) { // if the volume is LOUD?
-    // do something
-  //c = color(random(255), random(255), random(255)) ; // here I'm setting the background to a random color
-  state = 1;
-  }
-  break ;
+  switch (state) {
+    case 0:
+      background('red');
+      text("shhhh!", 250, 250);
+      if (vol > .05) { // if the volume is LOUD?
+        // do something
+        //c = color(random(255), random(255), random(255)) ; // here I'm setting the background to a random color
+        state = 1;
+      }
+      break;
 
-  case 1:
-  background('green');
-  text("QUIET DOWN!", 250, 250);
-  if (vol < .05) {
-  state = 0;
-  }
-  break ;
+    case 1:
+      background('green');
+      text("QUIET DOWN!", 250, 250);
+      if (vol < .05) {
+        state = 0;
+      }
+      break;
 
-}
+  }
   // extra stuff for debugging
   textSize(18);
   text("Click the screen first to give\npermission for mic input.\nMy volume is " + vol, 10, 60);
