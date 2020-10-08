aa1d51cc0f34e787780c1e018e8ef518dfa74fc0
rieger22 <58442709+rieger22@users.noreply.github.com>
2020-10-07 22:41:23 -0500

    7

--- p5/Unit2/mp2/sketch.js
+++ p5/Unit2/mp2/sketch.js
@@ -1,8 +1,9 @@
 var mic;
 var vol;
 var img1, img2, img3;
+var x = 0;
 let state = 0;
-let x = 0;
+//let x = 0;
 
 function setup() {
   createCanvas(500, 500);
@@ -18,49 +19,78 @@ function setup() {
 
 function draw() {
 
-vol = (mic.getLevel()).toFixed(2);
+  vol = (mic.getLevel()).toFixed(2);
 
   switch (state) {
     case 0:
-    background('black');
-    textAlign(CENTER);
-    fill('white');
-    textSize(24);
-    text("Make some noise for your favorite band!\nClick to start.", 250, 250);
-    if (vol > .03) {
-      state = 1;
-    }
-    break ;
+      background('black');
+      textAlign(CENTER);
+      fill('white');
+      textSize(24);
+      translate(x, 0);
+      text("Make some noise for your favorite band!\nClick to start.", x, 250);
+      x += 2;
+      if (x > width) {
+        x = -50;
+      }
+
+      if (vol > .03) {
+        state = 1;
+      }
+      break;
 
 
     case 1:
-    background('purple');
-    textAlign(CENTER);
-    textSize(32);
-    text("Shiner", 250, 250);
-    image(img1, width / 2, height / 2, 200, 200);
-    if (vol > .05) {
-      state = 2;
-    }
-    break ;
+      background('purple');
+      textAlign(CENTER);
+      fill('yellow');
+      textSize(32);
+      text("Shiner\nEhhh, good start I guess", 250, 450);
+
+      translate(x, 0);
+      image(img1, width / 2, height / 2, 400, x);
+      x += 3;
+      if (x > width) {
+        x = -500;
+      }
+
+      if (vol > .05) {
+        state = 2;
+      }
+      break;
 
     case 2:
-    background('red');
-    textAlign(CENTER);
-    textSize(32);
-    text("HUM", 250, 250);
-    if (vol > .1) {
-      state = 3;
-    }
-    break ;
+      background('red');
+      textAlign(CENTER);
+      fill('blue');
+      textSize(32);
+      text("HUM\nI'm glad you're trying now", 250, 450);
+
+      image(img2, width / 2, height / 2, x, 300);
+      x += 3;
+      if (x > width) {
+        x = -500;
+      }
+
+      if (vol > .1) {
+        state = 3;
+      }
+      break;
 
     case 3:
-    background('yellow');
-    textAlign(CENTER);
-    fill('purple');
-    textSize(32);
-    text("POSTER CHILDREN!!!", 250, 250);
-    break ;
+      background('yellow');
+      textAlign(CENTER);
+      fill('purple');
+      textSize(32);
+      image(img3, width / 2, height / 2, 400, 300);
+      translate(x, 0);
+
+      text("POSTER CHILDREN!!\nYOU DID IT", x, 450);
+      x += 2;
+      if (x > width) {
+        x = -75;
+      }
+      break;
   }
 
 }
@@ -68,10 +98,10 @@ vol = (mic.getLevel()).toFixed(2);
 function mouseReleased() {
   state++;
   if (state > 3) {
-    state = 0 ;
+    state = 0;
   }
 }
 
-  function touchStarted() {
-    getAudioContext().resume();
+function touchStarted() {
+  getAudioContext().resume();
 }
